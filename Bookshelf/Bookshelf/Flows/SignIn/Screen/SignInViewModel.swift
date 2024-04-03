3e746afcec7d4604c221146ec2ed3c260a8ee1a9
kerikgg <kirill.abramov.24@mail.ru>
2024-04-03 11:05:32 +0300

    Revert "Merge pull request #5 from KamilKhairutdinov/Abramov_task1_signIn"
    This reverts commit a7603e7f7da4e1cd40db52fdfe5c991e3869329b, reversing
    changes made to 550673036fd0cca85bc480542dc7058d892ac2b1.

--- Bookshelf/Bookshelf/Flows/SignIn/Screen/SignInViewModel.swift
+++ Bookshelf/Bookshelf/Flows/SignIn/Screen/SignInViewModel.swift
@@ -11,30 +11,10 @@ import Foundation
 class SignInViewModel {
     var validationError: Observable<String>
     var isSuccessfullyLoggedIn: Observable<Bool>
-    var firebaseError: Observable<String>
-    private let signInService = SignInService()
 
     init() {
         validationError = Observable("")
         isSuccessfullyLoggedIn = Observable(false)
-        firebaseError = Observable("")
     }
-
-    func signInUser(email: String?, password: String?) {
-        guard let email, let password else { return }
-        validationError.value = ""
-        if email.isEmpty || password.isEmpty {
-            validationError.value = "Поля не должны быть пустыми"
-        } else {
-            signInService.signIn(email: email, password: password) { [weak self] result in
-                guard let self else { return }
-                switch result {
-                case .success:
-                    self.isSuccessfullyLoggedIn.value = true
-                case .failure(let error):
-                    firebaseError.value = error.localizedDescription
-                }
-            }
-        }
-    }
+    // TODO: реализовать
 }
