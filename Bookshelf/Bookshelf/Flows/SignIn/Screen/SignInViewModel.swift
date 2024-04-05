ba44f12d1e403d12ee1bbb6516c95372b131d884
kerikgg <kirill.abramov.24@mail.ru>
2024-04-05 19:43:01 +0300

    add signIn logic

--- Bookshelf/Bookshelf/Flows/SignIn/Screen/SignInViewModel.swift
+++ Bookshelf/Bookshelf/Flows/SignIn/Screen/SignInViewModel.swift
@@ -11,10 +11,30 @@ import Foundation
 class SignInViewModel {
     var validationError: Observable<String>
     var isSuccessfullyLoggedIn: Observable<Bool>
+    var firebaseError: Observable<String>
+    private let signInService = SignInService()
 
     init() {
         validationError = Observable("")
         isSuccessfullyLoggedIn = Observable(false)
+        firebaseError = Observable("")
     }
-    // TODO: реализовать
+
+    func signInUser(email: String?, password: String?) {
+        guard let email, let password else { return }
+        validationError.value = ""
+        if email.isEmpty || password.isEmpty {
+            validationError.value = "Поля не должны быть пустыми"
+        } else {
+            signInService.signIn(email: email, password: password) { [weak self] result in
+                guard let self else { return }
+                switch result {
+                case .success:
+                    self.isSuccessfullyLoggedIn.value = true
+                case .failure(let error):
+                    firebaseError.value = error.localizedDescription
+                }
+            }
+        }
+    }
 }
---
+++ Bookshelf/Bookshelf/Services/SignInService.swift
@@ -0,0 +1,28 @@
+//
+//  SignInService.swift
+//  Bookshelf
+//
+//  Created by kerik on 05.04.2024.
+//
+
+import Foundation
+import FirebaseAuth
+
+// MARK: - Абрамов Кирилл
+class SignInService: SignInServiceProtocol {
+    private let auth = Auth.auth()
+
+    func signIn(
+        email: String,
+        password: String,
+        completion: @escaping (Result<User, Error>) -> Void
+    ) {
+        auth.signIn(withEmail: email, password: password) { result, error in
+            if let result = result {
+                completion(.success(result.user))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+}
